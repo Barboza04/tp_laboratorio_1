5c6974af1f72ab2a6bd42fee685b225a385ca5d4
Alejandro Barboza <barbozaalejandro@outlook.com.ar>
2016-05-27 11:14:05 -0300

    Add files via upload

--- TP_3/main.c
+++ TP_3/main.c
@@ -1,3 +1,14 @@
+/********************************************************************
+*Programa: TP 3- Cartelera de cine
+*
+*Objetivo:
+          Realizar un programa que permita agregar, borrar y modificar
+          peliculas en un archivo binario
+*
+*Versión 1.0 27/05/2016
+*Autor: Alejandro Barboza
+*
+********************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
